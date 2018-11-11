ec79e6539131480945de20d9a8296bf08e626758
Carter Steffen <cartste3@gmail.com>
2018-11-10 21:04:32 -0600

    Setup MongoDB

--- app/src/main/java/com/strategicinvestor/strategicinvestor/CompanyActivity.java
+++ app/src/main/java/com/strategicinvestor/strategicinvestor/CompanyActivity.java
@@ -3,6 +3,14 @@ package com.strategicinvestor.strategicinvestor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompanyActivity extends AppCompatActivity {
 
     @Override
@@ -11,6 +19,25 @@ public class CompanyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_company);
 
         LineChart chart = (LineChart) findViewById(R.id.chart);
+        /*
+        YourData[] dataObjects = ...;
+
+        List<Entry> entries = new ArrayList<Entry>();
+
+        for (YourData data : dataObjects) {
+
+            // turn your data into Entry objects
+            entries.add(new Entry(data.getValueX(), data.getValueY()));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet.setColor(...);
+        dataSet.setValueTextColor(...);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
+        */
     }
 
 }
