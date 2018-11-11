7f999f66af02f9879762ce5c23c1001cb7059c7d
Matthew Wen <mattwen2018@gmail.com>
2018-11-11 01:59:41 -0500

    ignore

--- app/src/main/java/com/strategicinvestor/strategicinvestor/fragment/PersonalFragment.java
+++ app/src/main/java/com/strategicinvestor/strategicinvestor/fragment/PersonalFragment.java
@@ -1,7 +1,10 @@
-package com.strategicinvestor.strategicinvestor;
+package com.strategicinvestor.strategicinvestor.fragment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.reimaginebanking.api.nessieandroidsdk.NessieError;
 import com.reimaginebanking.api.nessieandroidsdk.NessieResultsListener;
@@ -9,26 +12,37 @@ import com.reimaginebanking.api.nessieandroidsdk.constants.TransactionMedium;
 import com.reimaginebanking.api.nessieandroidsdk.models.Deposit;
 import com.reimaginebanking.api.nessieandroidsdk.models.PostResponse;
 import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
+import com.strategicinvestor.strategicinvestor.R;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class PersonalFinance extends AppCompatActivity {
-    NessieClient client = NessieClient.getInstance("d3647dccce6ddbbb8366ddbc5f747710");
+public class PersonalFragment extends Fragment {
 
-    SimpleDateFormat formatL = new SimpleDateFormat("yyyy-mm-dd", Locale.US);
-    public String currDate = formatL.format(new Date());
+    public PersonalFragment() {
+        // Required empty public constructor
+    }
+
+    public static PersonalFragment newInstance() {
+        return new PersonalFragment();
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_finance);
-        capitalOneDeposit(1200);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_personal, container, false);
+        capitalOneDeposit(12000);
+        return view;
     }
 
     public void capitalOneDeposit(Integer deposit_amount) {
+        NessieClient client = NessieClient.getInstance("d3647dccce6ddbbb8366ddbc5f747710");
+        SimpleDateFormat formatL = new SimpleDateFormat("yyyy-mm-dd", Locale.US);
+        String currDate = formatL.format(new Date());
+
         Deposit deposit = new Deposit.Builder()
                 .amount(deposit_amount)
                 .description("DEPOSIT")
