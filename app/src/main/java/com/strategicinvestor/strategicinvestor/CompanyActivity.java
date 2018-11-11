9e4421caec57c823fb0d1f77d79844b2300a19ef
Carter Steffen <cartste3@gmail.com>
2018-11-10 22:46:38 -0600

    Setup data to be converted to graph

--- app/src/main/java/com/strategicinvestor/strategicinvestor/CompanyActivity.java
+++ app/src/main/java/com/strategicinvestor/strategicinvestor/CompanyActivity.java
@@ -3,6 +3,15 @@ package com.strategicinvestor.strategicinvestor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 //import com.github.mikephil.charting.charts.LineChart;
 //import com.github.mikephil.charting.data.Entry;
 //import com.github.mikephil.charting.data.LineData;
@@ -18,9 +27,9 @@ public class CompanyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company);
 
-        //LineChart chart = (LineChart) findViewById(R.id.chart);
-        /*
-        YourData[] dataObjects = ...;
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+
+        ArrayList<Float> prices = new ArrayList<>();
 
         //YourData[] dataObjects = ...;
 
@@ -32,15 +41,14 @@ public class CompanyActivity extends AppCompatActivity {
             // turn your data into Entry objects
             entries.add(new Entry(prices.get(i), (float)(i)));
         }
-/*
+
         LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
-        dataSet.setColor(...);
-        dataSet.setValueTextColor(...);
+        //dataSet.setColor(...);
+        //dataSet.setValueTextColor(...);
 
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
         chart.invalidate(); // refresh
-        */
     }
 
 }
